f545123b22a93b940c32549d7cdf08ddbf07f729
Romain Cassignol <cassignol@secib.fr>
2020-03-02 12:06:28 +0100

    Updated docker env. setup script

--- setup.docker.wks.ps1
+++ setup.docker.wks.ps1
@@ -12,5 +12,5 @@ $dlg = New-Object System.Windows.Forms.FolderBrowserDialog
 $dlg.Description = "Select directory for cloning git repository";
 
 if ($dlg.ShowDialog() -eq "OK") {
-    Start-Process -WorkingDirectory $dlg.SelectedPath -FilePath $env:SystemRoot"\System32\cmd.exe" -ArgumentList ["/c echo Cloning git repository into %CD% && git clone https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker && explorer %CD%\Docker && exit"];
+    Start-Process -WorkingDirectory $dlg.SelectedPath -FilePath $env:SystemRoot"\System32\cmd.exe" -ArgumentList ["/k echo Cloning git repository into %CD% && git clone https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker && explorer %CD%\Docker"];
 }
