a908c95a38a1b4f9252fa1be41bb3ca3564949b4
Romain Cassignol <cassignol@secib.fr>
2020-03-02 11:49:06 +0100

    Fixed docker env. setup script

--- setup.docker.wks.ps1
+++ setup.docker.wks.ps1
@@ -10,8 +10,7 @@ choco install docker-desktop
 [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
 $dlg = New-Object System.Windows.Forms.FolderBrowserDialog
 $dlg.Description = "Select directory for cloning git repository";
-$dlg.RootFolder = "MyComputer"
 
 if ($dlg.ShowDialog() -eq "OK") {
-    Start-Process -WorkingDirectory $test.SelectedPath -FilePath $env:SystemRoot"\System32\cmd.exe" -ArgumentList ["/c echo Cloning git repository into %CD% && git clone https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker && explorer %CD%\Docker && exit"];
+    Start-Process -WorkingDirectory $dlg.SelectedPath -FilePath $env:SystemRoot"\System32\cmd.exe" -ArgumentList ["/c echo Cloning git repository into %CD% && git clone https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker && explorer %CD%\Docker && exit"];
 }
