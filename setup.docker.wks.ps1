3ce7b91d0d2af65314ac881bc6deaa312e25d40e
Romain Cassignol <cassignol@secib.fr>
2020-03-02 18:03:36 +0100

    Updated docker env. setup script

--- setup.docker.wks.ps1
+++ setup.docker.wks.ps1
@@ -7,4 +7,5 @@ choco install vscode
 Write-Host "Installing Docker Desktop"
 choco install docker-desktop
 Write-Host Prompting Visual Studio Code for git cloning
-Start-Process -FilePath $env:SystemRoot"\System32\cmd.exe" -ArgumentList "/c start cmd /c code --open-url -- vscode://vscode.git/clone?url=https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker"
+#Start-Process -FilePath $env:SystemRoot"\System32\cmd.exe" -ArgumentList "/c $env:ProgramW6432\Microsoft VS Code\code.exe --open-url -- vscode://vscode.git/clone?url=https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker"
+Start-Process -FilePath "$env:ProgramW6432\Microsoft VS Code\code.exe" -ArgumentList "--open-url -- vscode://vscode.git/clone?url=https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker"
