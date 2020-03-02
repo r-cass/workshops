6e678e632dbb3ef01e07a689b02233eb95d09fea
Romain Cassignol <cassignol@secib.fr>
2020-03-02 11:32:26 +0100

    Updated docker env. setup script to clone git repo

--- setup.docker.wks.ps1
+++ setup.docker.wks.ps1
@@ -7,4 +7,11 @@ choco install vscode
 Write-Host "Installing Docker Desktop"
 choco install docker-desktop
 
-git clone "https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker"
+[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+$dlg = New-Object System.Windows.Forms.FolderBrowserDialog
+$dlg.Description = "Select directory for cloning git repository";
+$dlg.RootFolder = "MyComputer"
+
+if ($dlg.ShowDialog() -eq "OK") {
+    Start-Process -WorkingDirectory $test.SelectedPath -FilePath $env:SystemRoot"\System32\cmd.exe" -ArgumentList ["/c echo Cloning git repository into %CD% && git clone https://azuredevops.septeo.fr/SECIB/Workshop/_git/Docker && explorer %CD%\Docker && exit"];
+}
